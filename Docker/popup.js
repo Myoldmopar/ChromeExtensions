beefa200119cf5c96dfa635657d77271e0d6cba7
Edwin Lee <leeed2001@gmail.com>
2015-05-29 08:14:59 -0600

    Add dummy docker extension; cleanup striker

---
+++ Docker/popup.js
@@ -0,0 +1,4 @@
+window.onload = function () {
+	document.getElementById("running").innerHTML = (Math.random()*40).toFixed(1);
+	document.getElementById("staged").innerHTML = (Math.random()*40).toFixed(1);
+}
--- Strikethrough/popup.js
+++ Strikethrough/popup.js
@@ -1,11 +1,5 @@
-window.onload = function () {
-	document.getElementById('strikeInput').onkeyup = function() { strike(); }; 
-	document.getElementById('strikeOutput').onclick = function() { document.getElementById('strikeOutput').select() }; 
-	document.getElementById('strikeInput').focus();
-};
-function strike() {
-	document.getElementById('strikeOutput').value = strikeString(document.getElementById('strikeInput').value, '\u0336');
-}
+
+// worker function for striking a string by applying a unicode non-printing character to each character in the string
 function strikeString(str, strikeKind) {
 	var result = '';
 	for (var i = 0; i < str.length; i++) {
@@ -15,3 +9,16 @@ function strikeString(str, strikeKind) {
 	}
 	return result;
 }
+
+// wrapper function to make striking easier
+function strike() {
+	document.getElementById('strikeOutput').value = strikeString(document.getElementById('strikeInput').value, '\u0336');
+}
+
+// things to do when the window loads
+window.onload = function () {
+	document.getElementById('strikeInput').onkeyup = function() { strike(); }; // set event handler to strike text when the text changes in the input box
+	document.getElementById('strikeOutput').onclick = function() { document.getElementById('strikeOutput').select() }; // set event handler to select the entire contents of the output box when clicked
+	document.getElementById('strikeInput').select(); // select the text in the input box to get it ready for typing directly into it
+	strike(); // and don't forget to strike the text to make it look nice when it opens
+};
